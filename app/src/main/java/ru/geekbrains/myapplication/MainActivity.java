f14f1b93cc255a963c320618fa180337f02ca8f6
turkovaliudmila <ljudmila327@rambler.ru>
2021-03-10 20:50:21 +0300

    Добавлены переменные для элементов кнопок

--- app/src/main/java/ru/geekbrains/myapplication/MainActivity.java
+++ app/src/main/java/ru/geekbrains/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package ru.geekbrains.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator_layout);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
     }
 }
