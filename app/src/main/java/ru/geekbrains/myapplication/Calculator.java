420c09fceb13cbfeac8663ca891808a4fd832ad4
turkovaliudmila <ljudmila327@rambler.ru>
2021-03-11 13:04:00 +0300

    В класс Calculator добавлена логика, связанная с вводом чисел и обновлением табло калькулятора

--- app/src/main/java/ru/geekbrains/myapplication/Calculator.java
+++ app/src/main/java/ru/geekbrains/myapplication/Calculator.java
@@ -1,16 +1,18 @@
 package ru.geekbrains.myapplication;
 
 public class Calculator {
-    private int inputMode; // 0 - ввод первого числа, 1 - ввод второго числа
+    private int inputMode; // 0 - ввод первого числа, 1 - ввод второго числа, 2 - получен результат
     private double firstNumber;
     private double secondNumber;
     private String operator;
+    private String textTablo;
 
     public Calculator() {
         inputMode    = 0;
         firstNumber  = 0;
         secondNumber = 0;
         operator     = "";
+        textTablo    = "";
     }
 
     public double getFirstNumber() {
@@ -59,4 +61,30 @@ public class Calculator {
         }
         return 0;
     }
+
+    public void inputNumber(String textCalc, int number) {
+        if (inputMode == 0) {
+            firstNumber = firstNumber * 10 + number;
+            textTablo = textCalc + number;
+        } else if (inputMode == 1) {
+            secondNumber = secondNumber * 10 + number;
+            textTablo = textCalc + number;
+        } else if (inputMode == 2) {
+            inputMode = 0;
+            firstNumber = number;
+            secondNumber = 0;
+            operator = "";
+            this.textTablo = "";
+            textTablo = "";
+        }
+    }
+
+    public String getTextTablo() {
+        return textTablo;
+    }
+
+    public void setTextTablo(String textTablo) {
+        this.textTablo = textTablo;
+    }
+
 }
