acd4205de07d71f56f6fac06f1b3e9cf05402e5f
turkovaliudmila <ljudmila327@rambler.ru>
2021-03-17 20:19:14 +0300

    В активити SettingsActivity добавлены слушатели для кнопок переключателей и кнопки Возврат

--- app/src/main/java/ru/geekbrains/myapplication/SettingsActivity.java
+++ app/src/main/java/ru/geekbrains/myapplication/SettingsActivity.java
@@ -2,13 +2,74 @@ package ru.geekbrains.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private static final String NAME_SHARED_PREFRENCES = "LOGIN";
+    private static final String appTheme = "MyStyle";
+
+    private static final int appThemeLight = 0;
+    private static final int appThemeDark = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button btnReturn = findViewById(R.id.ReturnButton);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        initThemeChooser();
+    }
+
+    private void initThemeChooser() {
+        initRadioButton(findViewById(R.id.RadioButtonMaterialLight), appThemeLight);
+        initRadioButton(findViewById(R.id.RadioButtonMaterialDark), appThemeDark);
+    }
+
+    private void initRadioButton(View button, final int codeStyle) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAppTheme(codeStyle);
+                recreate();
+            }
+        });
+    }
+
+    private int getAppTheme(int codeStyle) {
+        return codeStyleToStyleId(getCodeStyle(codeStyle));
+    }
+
+    private int getCodeStyle(int codeStyle) {
+        SharedPreferences sharedPreferences = getSharedPreferences(NAME_SHARED_PREFRENCES, MODE_PRIVATE);
+        return sharedPreferences.getInt(appTheme, codeStyle);
+    }
+
+    private int codeStyleToStyleId(int codeStyle) {
+        switch (codeStyle) {
+            case appThemeLight:
+                return R.style.AppThemeLight;
+            case appThemeDark:
+                return R.style.AppThemeDark;
+            default:
+                return R.style.MyStyle;
+        }
+    }
+
+    private void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPreferences = getSharedPreferences(NAME_SHARED_PREFRENCES, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(appTheme, codeStyle);
+        editor.apply();
     }
 }
